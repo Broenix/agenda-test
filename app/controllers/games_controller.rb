db5a2d21fe8f07f97a7cceff1a73442a92e4a488
theo chevalier <chevalie.theo@gmail.com>
2023-02-22 11:47:12 +0100

    On essaie l'index mais ça ne fonctionne pas

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,14 @@
 class GamesController < ApplicationController
+  require 'httparty'
+
   def index
+    response = HTTParty.get('https://api.twitch.tv/helix/games/top', headers: {
+      "u8mhg8u181nax39uf2k9sj5ttv7k6b" => ENV['TWITCH_CLIENT_ID']
+    })
+    if response.code == 200
+      @games = JSON.parse(response.body)['data']
+    else
+      puts "Erreur lors de l'appel API"
+    end
   end
 end
